293111f306e1faaf7882cf55c45553249523e3bf
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-04-13 19:50:10 +0300

    Create custom button

---
+++ Followers/FollowersButton.swift
@@ -0,0 +1,36 @@
+//
+//  FollowersButton.swift
+//  Followers
+//
+//  Created by AnatoliiOstapenko on 13.04.2022.
+//
+
+import UIKit
+
+class FollowersButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //custom code begins here
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // Create init to reuse button with differ title and color
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = .preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ Followers/FollowersTextField.swift
@@ -0,0 +1,20 @@
+//
+//  FollowersTextField.swift
+//  Followers
+//
+//  Created by AnatoliiOstapenko on 13.04.2022.
+//
+
+import UIKit
+
+class FollowersTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
