0de5609182e614903f9b268f5700af31b4ed025f
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-04-13 20:27:37 +0300

    Create custom textfield

--- Followers/FollowersTextField.swift
+++ Followers/FollowersTextField.swift
@@ -8,13 +8,32 @@
 import UIKit
 
 class FollowersTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderColor = UIColor.systemGray4.cgColor
+        layer.borderWidth = 2
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = .preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
 
 }
